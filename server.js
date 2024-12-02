58afdaeaa504261207bbc026d063bbec438a51e1
Oleksandr K. <alexsanqp@gmail.com>
2024-12-03 01:09:03 +0200

    Update server port to 3001

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', (req, res) => {
 });
 
 // Налаштування порту
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 // Запуск сервера
 app.listen(PORT, () => {
